d8d3865171bfe9d05984f2c298d9156ea1ca290d
Bob Peulen <75362416+bobpeulen@users.noreply.github.com>
2024-06-17 17:04:56 +0200

    Update part_1.py

--- part_1.py
+++ part_1.py
@@ -7,10 +7,7 @@ import random
 from json import loads, dumps
 from urllib.request import urlopen 
 import subprocess
-import pandas as pd
 from random import Random
-from retry import retry
-import concurrent.futures
 import argparse
 from opensearchpy import OpenSearch, RequestsHttpConnection
 from opensearchpy import helpers
